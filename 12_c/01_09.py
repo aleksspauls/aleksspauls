867e5a719d5adb6176d48540562ebf391a23382d
aleksspauls <90207187+aleksspauls@users.noreply.github.com>
2021-09-17 14:21:44 +0300

    17_09_stunda

---
+++ 12_c/01_09.py
@@ -0,0 +1,30 @@
+#uzd(1)
+
+gadalaiks = "rudens"
+sk1 = 1.414213
+
+#uzd(2)
+
+v = "Java"
+i = "ir"
+l = "augsta"
+a = "līmeņa programmēšana valoda."
+t = "Tās pirmā versija izlaista,"
+g = "1995.gadā,"
+p = "t.i. pirms 26 gadiem."
+print(v, i, l, a, t, g, p)
+# print(v, "ir", l, "līmeņa...
+
+#uzd(3)
+
+i =input( "Uzraksti savu Vārdu Uzvārdu")
+f = float(input("Tavs augums metros"))
+print (i, f)
+
+#uzd(4)
+
+cukursDZ1 = float(input("Cik daudz cukura 100 ml dzēriena? "))
+if cukursDZ1<2.5:
+    print("Zems cukura daudzums"
+
+augsnesPH = float(input("Ievadi augsnes skābuma līmeni "))
