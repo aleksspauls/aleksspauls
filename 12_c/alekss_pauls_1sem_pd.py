2027160a58d830d9fa52a426e24d54dc5fbff6e4
aleksspauls <90207187+aleksspauls@users.noreply.github.com>
2022-01-28 16:54:38 +0200

    p_d

---
+++ 12_c/alekss_pauls_1sem_pd.py
@@ -0,0 +1,41 @@
+#uzd1
+
+pr1 = ["Python", "Java"]
+pr2 = ["C", "JavaScript"]
+pr3 = ["Go", "R"]
+pr4 = ["PHP", "MATLAB", "Pascal"]
+tas = [pr1, pr2, pr3, pr4]
+bet = tas.sort
+print(bet)
+
+tas = [pr1, pr2, pr3, pr4]
+bet = tas.sort
+for i in range(bet):
+    remove(bet[6])
+
+tas2 = ["Swift"]
+tas.extend(tas2)
+print(tas)
+
+
+#uzd2
+
+saldejums = ["Ekselence", "Karlsons", "Pols", "Tio"]
+saldejums.sort
+print(saldejums)
+
+saldejums.sort(reverse = True)
+print(saldejums)
+
+
+#uzd3
+
+skaitli = [752,7436,4344,1061,1179,7451,900,2570,3960,6798,6605,8838,3144,16769,1375,2067,91913,9575,539,3803,7650,7462,2466,1965,23500,60804,57145,3763,2073,8488,11111,2250,6314,76618,1109,7393,7850,1655,8284,7537,1224,24765,11222,941,905,3221,6722,2051,30386,696986,2873,1528,2758,18972,10736,3370,1233,12268,2069,5354,1171,1728,1135,637,9937,18436,1303,5173,24967,3626,1862,38059,1600,1327,2995,1457]
+avg = sum.skaitli / len(skaitli)
+return avg
+
+tas = skaitli.sort
+if cipars < tas:
+    print("+ tas")
+else:
+  print("- tas")
