9eb31ce2d9a416ce7e76b9fb1b0ef8aeb3c3ffc9
aleksspauls <90207187+aleksspauls@users.noreply.github.com>
2022-03-07 14:23:56 +0200

    07_03

---
+++ 12_c/07_03.py
@@ -0,0 +1,8 @@
+def patskanis():
+    divskanis1 = "ei"
+    divskanis2 = "ai"
+    simboli = input("Ievadi simbolu virkni")
+    if simboli.count(divskanis1 == divskanis):
+        print("Ir")
+    else:
+        print("nav")
